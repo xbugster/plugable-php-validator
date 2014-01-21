a285fc294d8298e05c27cf4cb6a86d0e357b578f
Leader VR <leaderpvp@gmail.com>
2014-01-20 19:22:58 -0800

    Added psr-0 autoloader, testing index file.

---
+++ index-testing.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Index
+ * Simply for testing environment
+ * @author Valentin Ruskevych
+ */
+
+require_once(dirname(__DIR__).'/Environment/Autoloader.php');
+$autoloader = new Autoloader;
+$autoloader->register();
