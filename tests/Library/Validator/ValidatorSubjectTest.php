7dae8230a689c146e1445d56087e891d1076ac64
Valentin Ruskevych <valentinr@netvertise.co.il>
2014-06-29 13:43:38 +0300

    Added first test case.

---
+++ tests/Library/Validator/ValidatorSubjectTest.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @desc Test Case for Validator Subject
+ * @author Mr.X
+ *
+ * @since : 6/29/14
+ */
+
+class ValidatorSubjectTest extends PHPUnit_Framework_TestCase {
+    // Here will come setUp/tearDown and tests.
+}
